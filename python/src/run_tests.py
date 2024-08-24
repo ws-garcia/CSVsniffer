c64cd7029d7a3060e80f260da0a3e39b5f4b23bb
W. García <dark_slide90@hotmail.com>
2024-08-24 14:29:34 -0400

    Update run_tests.py

--- python/src/run_tests.py
+++ python/src/run_tests.py
@@ -42,7 +42,7 @@ def runsingleTest(threshold: int, \
                     test_sets=['CSV', 'W3C-CSVW', 'CSV_Wrangling', 'CSV_Wrangling', 'CSV_Wrangling']
                     )
 if __name__ == "__main__":
-     """
+     #"""
      runsingleTest(threshold=10, # Load 10 records
                    data_threshold=-1, 
                    detector='CSVsniffer') 
@@ -52,7 +52,10 @@ if __name__ == "__main__":
      runsingleTest(threshold=-1,
                    data_threshold=6144, # Load 6144 characters
                    detector='Python sniffer')
-     """ 
      runsingleTest(threshold=-1, 
                    data_threshold=-1, # Load all file
                    detector='CleverCSV') 
+    # """ 
+     #runsingleTest(threshold=10, # Load 10 records
+     #              data_threshold=-1, 
+     #              detector='CSVsniffer') 
